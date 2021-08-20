3165ead1611a5ec838ac3b3436b2726d42c4be92
Malinovskii Vladimir <galqiwi@galqiwi.ru>
2021-08-20 19:20:10 +0300

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+
+def calculator():
+    print('Введи запрос, а я его вычислю')
+    command = yield
+    try:
+        print(f'{command}={eval(command)}')
+    except SyntaxError:
+        print('Я не понял, о чём ты')
+
+
+def bot():
+    print('Привет, я бот, мои команды:')
+    print('1) Привет')
+    print('2) Калькулятор')
+    while True:
+        command = yield
+        if command == 'Привет':
+            print('Привет!')
+        elif command == 'Калькулятор':
+            yield from calculator()
+        else:
+            print('Не понимаю, о чём вы.')
+
+
+test_bot = bot()
+test_bot.send(None)
+
+while True:
+    try:
+        test_bot.send(input('>>> '))
+    except KeyboardInterrupt:
+        break
